68506c7371cc5b6ec94e515079f7ee4d10a0876c
ernests <ernests.dumbrauskis1@gmail.com>
2026-03-05 00:11:01 +0200

    Drawer component that will be used for the user list

---
+++ src/app/shared/components/drawer/drawer.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component, input } from "@angular/core";
+import { TailwindWidth } from "../../types/tailwind.types";
+
+@Component({
+    selector: 'app-drawer',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    template: `
+        <div [class]="currentWidth"
+            class="transition-all duration-300 h-full bg-white shadow-lg overflow-hidden">
+            <ng-content />
+        </div>
+    `,
+})
+export class DrawerComponent {
+    isExpanded = input<boolean>(false);
+
+    expandedWidth = input<TailwindWidth>('w-full');
+    collapsedWidth = input<TailwindWidth>('w-1/4');
+
+    currentWidth() {
+        return this.isExpanded() ? this.expandedWidth() : this.collapsedWidth();
+    }
+}
