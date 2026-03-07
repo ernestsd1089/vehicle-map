9109863b03d3962bb5bb68d7e715e8f1ea0fa0d9
ernests <ernests.dumbrauskis1@gmail.com>
2026-03-07 17:44:03 +0200

    adding close button to the vehicle panel and fixing image being cropped

--- src/app/features/vehicle-tracker/components/vehicle-details/vehicle-details.component.ts
+++ src/app/features/vehicle-tracker/components/vehicle-details/vehicle-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { switchMap, of } from 'rxjs';
@@ -9,18 +10,23 @@ import {
   selectSelectedVehicle,
   selectSelectedVehicleLocation,
 } from '../../store/vehicle-data/vehicle-data.reducer';
+import { VehicleDataActions } from '../../store/vehicle-data/vehicle-data.actions';
 import { GeocodingService } from '../../../../shared/services/geocoding.service';
 
 @Component({
   selector: 'app-vehicle-details',
   templateUrl: './vehicle-details.component.html',
-  imports: [MatIconModule, MatDividerModule],
+  imports: [MatIconModule, MatIconButton, MatDividerModule],
 })
 export class VehicleDetailsComponent {
   private readonly store = inject(Store);
   private readonly geocoding = inject(GeocodingService);
 
   vehicle = this.store.selectSignal(selectSelectedVehicle);
+
+  close() {
+    this.store.dispatch(VehicleDataActions.deselectVehicle());
+  }
 
   address = toSignal(
     toObservable(this.store.selectSignal(selectSelectedVehicleLocation)).pipe(
